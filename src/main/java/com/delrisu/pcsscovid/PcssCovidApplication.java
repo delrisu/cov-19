dbfa080b2d7c0a35cc29a7c956e77db0789819b7
Robert Ciemny <emeryt360@gmail.com>
2021-04-10 15:48:26 +0200

    Initial Commit

---
+++ src/main/java/com/delrisu/pcsscovid/PcssCovidApplication.java
@@ -0,0 +1,13 @@
+package com.delrisu.pcsscovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcssCovidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcssCovidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delrisu/pcsscovid/PcssCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delrisu.pcsscovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcssCovidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
